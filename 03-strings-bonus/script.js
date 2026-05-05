542aeccadd8d0d630248b3e8d91841e889c0a794
Emanuele Favero <emafaux89@gmail.com>
2026-05-06 01:53:53 +0200

    docs: add alternative solutions in 03-strings-bonus

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,17 +6,22 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-// Metodo A
+// Soluzione Alternativa: city.search('Y')
 const cityIndex = city.indexOf('Y');
 console.log(cityIndex); // Output: 4
 
-// 2. Sottostringhe
-// Stampa la sottostringa "York" dalla variabile city
+// 2. Sotto-stringhe
+// Stampa la sotto-stringa "York" dalla variabile city
+// Soluzioni Alternative:
+// city.slice(4)
+// city.substring(4, 8)
+// city.substring(city.indexOf('Y'))
 const citySubstring = city.substring(4);
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
+// Soluzione Alternativa: city.replace(citySubstring, 'Delhi')
 const cityReplaced = city.replace('York', 'Delhi');
 console.log(cityReplaced); // Output:  "New Delhi"
 
@@ -26,7 +31,7 @@ console.log(cityReplaced); // Output:  "New Delhi"
 const cityNameConcat = `${city} ${cityName}`;
 console.log(cityNameConcat); // Output: "New York City"
 
-// 5. Tutto in maiscolo
-// Trasforma tutto in maiscuolo la stringa della variabile hello
+// 5. Tutto in maiuscolo
+// Trasforma tutto in maiuscolo la stringa della variabile hello
 const upperCaseHello = hello.toUpperCase();
 console.log(upperCaseHello); // Output: "HELLO"
